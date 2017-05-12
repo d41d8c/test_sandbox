3b288481b3ce5ffc7d7ddba2024c92a2f87d05e5
ivan.konstantinovskyi <ivan.konstantinovskyi@motorolasolutions.com>
2017-05-12 12:37:05 +0200

    modified structures

--- index.py
+++ index.py
@@ -1,4 +1,11 @@
 import time
-print ("python endnedeads f a")
 
-time.sleep(5)
+
+def main():
+	print ("python endnedeads f a")
+
+	time.sleep(5)
+	print ('olol')
+	return 0
+if __name__=='__main__':
+	main()
